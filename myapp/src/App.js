9e39efbd96460ec51d3f6fc201ebda06c44ca0a6
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-26 09:00:37 +0000

    About.js created

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
-import{BrowseRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom'
+import{BrowserRouter as Router,Routes,Link,navLink,Route} from 'react-router-dom';
+
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
     <div class name="App" >
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path='/home' element={<Home/>}/>
         <Route path='/contact' element={<Contact/>}/>
--- myapp/src/components/Contact.js
+++ myapp/src/components/Contact.js
@@ -1,3 +1,7 @@
-export default function(){
-    
+export default function Contact(){
+    return(
+        <div>
+       <h1>Hello</h1>
+       </div>
+    )
 }
